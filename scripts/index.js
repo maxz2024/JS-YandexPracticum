aedb89c906f934568ea7a223c9e1be78b1bf8786
maxz2024 <maksimzdonov456@yandex.ru>
2023-12-09 20:46:56 +0300

    Add sounds for button card

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,13 @@
 let sneaker = document.querySelector('.sneaker');
 console.log(sneaker) 
+
+let buttons = document.querySelectorAll(".sneakerButton");
+console.log(buttons)
+function butClick() {
+    let audio = new Audio("/music/Звук Когда девушке стало приятно.mp3")
+    audio.play()
+}
+for (let i = 0; i< buttons.length; i++){
+    buttons[i].onclick = butClick
+
+}
